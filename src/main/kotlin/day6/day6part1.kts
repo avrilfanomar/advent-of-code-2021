2e77dbb70528da286e6118ebe797c900e33aab90
Andrii Andriichuk <a.andriichuk@douglas.de>
2021-12-22 12:47:02 +0100

    day 6 part 1

---
+++ src/main/kotlin/day6/day6part1.kts
@@ -0,0 +1,22 @@
+package day5
+
+import java.io.File
+
+val daysToLive = 80
+
+var fishAgeList = mutableListOf<Int>()
+File("input.txt").readLines()[0].split(",").map { it.toInt() }.forEach { fishAgeList.add(it) }
+for (i in 1..daysToLive) {
+    val newFishAgeList = mutableListOf<Int>()
+    fishAgeList.forEach() {
+        if (it == 0) {
+            newFishAgeList.add(8)
+            newFishAgeList.add(6)
+        } else {
+            newFishAgeList.add(it - 1)
+        }
+    }
+    fishAgeList = newFishAgeList
+}
+
+println(fishAgeList.size)
